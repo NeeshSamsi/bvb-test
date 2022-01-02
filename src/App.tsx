8ff8297bc31dab2d7973d72c26447b255a771430
Neesh <avaneeshsamsi@gmail.com>
2022-01-02 22:51:39 +0530

    feat: :sparkles: Basic implementation of Component to PDF Print

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+// import domToPdf from "react-dom-to-pdf";
+import { useRef } from "react";
+import { useReactToPrint } from "react-to-print";
+
+import Test from "./components/TestPDF";
+
 function App() {
+  // const element = document.getElementById("pdf");
+  // const options = {
+  //   filename: "myPDF.pdf",
+  // };
+
+  const componentRef = useRef(null);
+
+  const downloadPdf = useReactToPrint({
+    content: () => componentRef.current,
+  });
+
+  // const downloadPdf = () => {
+  //   domToPdf(element, options);
+  // };
+
   return (
-    <div className="App">
-      <h1>Hewwo React!</h1>
+    <div className="flex flex-col justify-center items-center">
+      <Test ref={componentRef} />
+      <button className="bg-gray-400 text-black rounded-md py-2 px-4" onClick={downloadPdf}>
+        Download as PDF
+      </button>
     </div>
   );
 }
